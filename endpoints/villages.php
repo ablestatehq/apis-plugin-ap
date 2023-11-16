7fffb3f298110c5abd8c9056bc8b2921d9620d10
David Wampamba <davidwampamba@gmail.com>
2023-11-16 23:14:24 +0300

    Meta fields for village

--- endpoints/villages.php
+++ endpoints/villages.php
@@ -12,12 +12,22 @@
         $the_villages = array();
 
         foreach( $villages as $village) {
+            $region = get_post_meta( $village->ID, 'region',true);
+            $district = get_post_meta( $village->ID, 'district',true);
+            $county = get_post_meta( $village->ID, 'county',true);
+            $sub_county = get_post_meta( $village->ID, 'sub_county',true);
+            $parish = get_post_meta( $village->ID, 'parish',true);
             $sub_parish = get_post_meta( $village->ID, 'sub_parish',true);
             $sub_parish_post = get_post( $sub_parish );
             $sub_parish_name = $sub_parish_post->post_title;
             $the_villages[] = array(
                 'id' => $village->ID,
                 'name' => $village->post_title,
+                'region' => $region,
+                'district' => $district,
+                'county' => $county,
+                'sub_county' => $sub_county,
+                'parish'=> $parish,
                 'sub_parish' => (object) array(
                     'id' =>$sub_parish_name,
                     'name' => $sub_parish_name
