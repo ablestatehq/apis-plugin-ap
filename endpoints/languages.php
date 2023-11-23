7b9eda40007e17bf61301c6af75b755bbe5c9dda
David Wampamba <davidwampamba@gmail.com>
2023-11-23 22:51:56 +0300

    Refactor language endpoint

--- endpoints/languages.php
+++ endpoints/languages.php
@@ -13,20 +13,11 @@
         $the_languages = array();
 
         foreach( $languages as $language) {
-            $tribe_id = get_post_meta( $language->ID, 'tribe', true )[0];
-
-            if( $tribe_id ) {
-                $tribe_post = get_post( $tribe_id );
-                $tribe_name = $tribe_post->post_title;
-
-            }
+            $tribe_id = (int)get_post_meta( $language->ID, 'tribe', true )[0];
+            $tribe_name = get_the_title( $tribe_id );
 
             $region_of_origin_id = (int) get_post_meta( $language->ID, 'region_of_origin', true)[0];
-
-            if( $region_of_origin_id ) {
-                $region_of_origin_post = get_post( $region_of_origin_id );
-                $region_of_origin_name = $region_of_origin_post->post_title;
-            }
+            $region_of_origin_name = get_the_title( $region_of_origin_id );
 
             $international = get_post_meta( $language->ID, 'international', true);
             $regional = get_post_meta( $language->ID, 'regional', true);
@@ -36,19 +27,19 @@
             $the_languages[] = array(
                 'id' => $language->ID,
                 'name' => $language->post_title,
-                'tribe' => $tribe_id ? (object) [
-                    'id' => $tribe_id,
-                    'name' => $tribe_name,
-                ]
-                : null,
+                'tribe' => (object) [
+                    "id" => $tribe_id > 0 ? $tribe_id : null,
+                    'name' => !empty($tribe_name) ? $tribe_name: "",
+                ],
+                'region_of_origin' => (object) [
+                    'id' => $region_of_origin_id > 0 ? $region_of_origin_id : null,
+                    'name' => !empty($region_of_origin_name) ? $region_of_origin_name : "",
+                ],
                 'international' => $international === "1" ? "yes" : "no",
                 'regional' => $regional=== "1" ? "yes" : "no",
                 'national' => $national === "1" ? "yes" : "no",
                 'endangered' => $endangered === "1" ? "yes" : "no",
-                'region_of_origin' => (object) [
-                    'id' => $region_of_origin_id,
-                    'name' => $region_of_origin_name,
-                ]
+
             );
         }
 
